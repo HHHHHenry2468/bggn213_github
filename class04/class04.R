be53fce0068fe00565d6e72f05a37eafd5a61c57
Henry Li <xil109@ucsd.edu>
2025-11-14 14:55:48 -0800

    Updating all previous classes

---
+++ class04/class04.R
@@ -0,0 +1,16 @@
+#' ---
+#' title: "Class 4 Lab"
+#' author: "Xiaohe Li (PID 16354124)"
+#' date: "Oct 10th, 2025"
+#' ---
+# This is my first R script
+x <- 1:50
+plot(x)
+plot(x, col='purple')
+plot(x, sin(x), col='orange')
+plot(x, sin(x), col='orange', type='l')
+plot(x, sin(x), col='orange', type='l', lwd=3)
+
+log(10)
+log10(10)
+log(10, base=exp(1))
